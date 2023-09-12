7e6fb2e86d3ded76d4875bf8c00a1025b6391790
fming <1487792164@qq.com>
2023-09-12 23:30:18 +0800

    feat: 创建新项目

---
+++ src/main/java/com/example/loginproject/LoginProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProjectApplication.class, args);
+	}
+
+}
